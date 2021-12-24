00cd2b0eea592a8df04a4808a55ac92e1f9e973e
romerolaia <93878330+romerolaia@users.noreply.github.com>
2021-12-24 20:28:41 +0100

    updated js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let date = new Date();
-let h5 = document.querySelector("h5");
 let days = [
   "Sunday",
   "Monday",
@@ -12,8 +11,11 @@ let days = [
 let day = days[date.getDay()];
 let hour = date.getHours();
 let minute = date.getMinutes();
-h5.innerHTML = `${day}, ${hour}:${minute}`;
 
+function setTime(time) {
+  let h5 = document.querySelector("h5");
+  h5.innerHTML = `${day}, ${hour}:${minute}`;
+}
 function displayWeatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -38,10 +40,10 @@ function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#inputPassword6").value;
   searchCity(city);
-}
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
 
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
+}
 function searchLocation(position) {
   let apiKey = "9089811745286b14b5b1117730d09f30";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -51,6 +53,8 @@ function searchLocation(position) {
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+  let currentLocationButton = document.querySelector("#location-button");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
 }
 
 function fahrenheit(event) {
@@ -64,8 +68,5 @@ function celsius(event) {
   let celsiusTemp = document.querySelector("#city-temperature");
   celsiusTemp.innerHTML = "⛅ 17";
 }
-
-let currentLocationButton = document.querySelector("#location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Barcelona");
